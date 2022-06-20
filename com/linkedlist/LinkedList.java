3f902c8a73d13110dbfc7d37f867acf08a76cd8b
MANOJ-51 <m4anoj51@gmail.com>
2022-06-20 13:44:02 +0530

    displaying welcome message

---
+++ com/linkedlist/LinkedList.java
@@ -0,0 +1,8 @@
+package com.linkedlist;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        //displaying welcome message
+        System.out.println("Welcome to linked list ");
+    }
+}
